9ac90a2dc77f3a6364add3bcbcde607cba698cba
krimamadhikarmi <madhikrima20@gmail.com>
2026-03-24 17:36:17 +0545

    my service section removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
         <HeroSection />
         <Skills />
         <Education />
-        <MyServices />
         <MyProjects />
         <Contact />
       </div>
