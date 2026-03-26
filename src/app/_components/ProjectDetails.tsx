787ef468da83b3d03322eb7747750b5284c66f9c
krimamadhikarmi <madhikrima20@gmail.com>
2026-03-26 21:41:16 +0545

    myproject section responsive

--- src/app/_components/ProjectDetails.tsx
+++ src/app/_components/ProjectDetails.tsx
@@ -9,27 +9,31 @@ interface ProjectDetailsProps {
 
 export function ProjectDetails({ imgUrl, title, gitUrl }: ProjectDetailsProps) {
   return (
-    <div className="px-8">
-      <div
-        className="h-64 sm:h-72 md:h-80 rounded-t-xl relative group "
-        style={{
-          background: `url(${imgUrl})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="overlay  items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-          <Link
-            href={gitUrl}
-            target="_blank"
-            className="flex items-center justify-center h-16 w-16 border-2 relative rounded-full border-[#ADB7BE] hover:border-white"
-          >
-            <CodeBracketIcon className="h-14 w-14 text-[#ADB7BE] cursor-pointer" />
-          </Link>
+    <div className="">
+      <div className="rounded-xl shadow-sm hover:shadow-lg hover:shadow-[#99a1a7] transition duration-300 shadow-[#ADB7BE] flex flex-col h-full">
+        {" "}
+        <div
+          className="w-full h-56 sm:h-64 md:h-68 lg:h-80 rounded-t-xl relative group overflow-hidden"
+          style={{
+            background: `url(${imgUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+            <Link
+              href={gitUrl}
+              target="_blank"
+              className="flex items-center justify-center h-16 w-16 border-2 rounded-full border-[#ADB7BE] hover:border-white"
+            >
+              <CodeBracketIcon className="h-14 w-14 text-[#ADB7BE]" />
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="text-white rounded-b-xl bg-[#181818] py-4 px-4 text-center">
-        <h5 className="text-lg font-semibold mb-2">{title}</h5>
+        <div className="text-white rounded-b-xl bg-[#181818] py-4 px-4 text-center flex flex-col justify-center flex-grow">
+          {" "}
+          <h5 className="text-lg font-semibold mb-2">{title}</h5>
+        </div>
       </div>
     </div>
   );
--- src/app/_components/ProjectList.tsx
+++ src/app/_components/ProjectList.tsx
@@ -12,7 +12,7 @@ interface Project {
 const ProjectList = ({ projects,limitProjects}:{projects: Project[],limitProjects: boolean}) => {
   const displayedProjects = limitProjects ? projects.slice(0, 3) : projects;
   return (
-    <div className="mb-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="mb-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6   lg:gap-6 px-8 sm:px-6 lg:px-6 xl:px-8">
       {displayedProjects.map((project) => (
         <ProjectDetails
           key={project.id}
